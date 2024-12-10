ff3970567ff88ef8c026d3a69fa428c0ad536995
zeyucui1 <loading1261@gmail.com>
2024-12-10 12:58:09 +1100

    chore: format code for consistency and readability

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache'
 import { auth, signIn, signOut } from './auth'
 import { supabase } from './supabase'
+import { getBookings } from './data-service'
 
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' })
@@ -11,7 +12,21 @@ export async function signInAction() {
 export async function signOutAction() {
   await signOut({ redirectTo: '/' })
 }
+// detele reservation
+export async function deleteReservation(bookingId) {
+  const session = await auth()
+  if (!session) throw new Error('Unauthorized')
+  const guestBookings = await getBookings(session.user.guestId)
+  const guestBookingIds = guestBookings.map((booking) => booking.id)
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error('Unauthorized: booking does not belong to this guest')
+  }
 
+  const { error } = await supabase.from('bookings').delete().eq('id', bookingId)
+
+  if (error) throw new Error('Booking could not be deleted')
+  revalidatePath('/account/reservations')
+}
 // update guest profile in nextjs Server component
 export async function updateGuest(formData) {
   const session = await auth()
